a2b4da05bdb4f3124b2e54be42ccf0ed82c7c6ed
jihyunna <skwlgus106@naver.com>
2022-11-26 15:31:10 +0900

    게시물 포스팅 1차

--- app/src/main/java/com/example/snsproject/MainActivity.kt
+++ app/src/main/java/com/example/snsproject/MainActivity.kt
@@ -3,45 +3,93 @@ package com.example.snsproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
 import com.example.snsproject.databinding.ActivityMainBinding
+import com.example.snsproject.navigation.AlarmFragment
+import com.example.snsproject.navigation.DetailViewFragment
+import com.example.snsproject.navigation.GridFragment
+import com.example.snsproject.navigation.UserFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import android.Manifest
+import android.content.pm.PackageManager
+import android.view.Menu
+import androidx.core.content.ContextCompat
+import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
+    lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        binding.bottomNavigation.setOnItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+        binding.bottomNavigation.selectedItemId = R.id.action_home
+
+        // login
         if (Firebase.auth.currentUser == null) {
             startActivity(
-                Intent(this, LoginActivity::class.java))
+                Intent(this, LoginActivity::class.java)
+            )
             finish()
         }
-
-        //binding.textUID.text = Firebase.auth.currentUser?.uid ?: "No User"
 
         binding.signout.setOnClickListener {
             Firebase.auth.signOut()
             startActivity(
-                Intent(this, LoginActivity::class.java))
+                Intent(this, LoginActivity::class.java)
+            )
             finish()
         }
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.storage -> startActivity(
-                Intent(this, StorageActivity::class.java))
-            R.id.remote_config -> startActivity(
-                Intent(this, RemoteConfigActivity::class.java))
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                //if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this, PostingActivity::class.java))
+               // }
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true
+            }
         }
-        return super.onOptionsItemSelected(item)
+        return false
     }
 }
